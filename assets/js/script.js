2841583a9c8f4e2fa8cf4674527b4a17053c88a6
Harry Shah <harvinder.shah@gmail.com>
2021-07-07 11:41:06 -0400

    added arrays to store characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,11 +11,20 @@ var generatePassword = function() {
   if (passwordLength<8 || passwordLength>128) {
     prompt("This is invalid number. Please enter a number between 8 and 128");
   };
-  console.log(passwordLength);
-
+  
   var includeUpperCase = confirm("Do you want to include uppercase?");
   var includeNumbers = confirm("Do you want to include numbers")
   var includeSpecicalCharacters = confirm("Do you want to include special characters like ! # @ $ % ?");
+
+  // create array for storing different characters
+
+  var lowerCaseAlphabets = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var upperCaseAlphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  var specialChar = ["/", "[", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "[", "]", "{", "}", ";", ":", "\\", "|", ",", ".", "<", ">", "/", "?", "]", "+", "/"]
+
+ 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
