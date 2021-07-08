60123db8ac748501ca4f231c8e34420b5785da5c
Harry Shah <harvinder.shah@gmail.com>
2021-07-08 16:25:15 -0400

    generate password function modified

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,47 +11,47 @@ var generatePassword = function() {
   if (passwordLength<8 || passwordLength>128) {
     prompt("This is invalid number. Please enter a number between 8 and 128");
   };
-  
+  console.log(passwordLength);
+
   var includeUpperCase = confirm("Do you want to include uppercase?");
   var includeNumbers = confirm("Do you want to include numbers")
   var includeSpecicalCharacters = confirm("Do you want to include special characters like ! # @ $ % ?");
 
   // create array for storing different characters
 
-  var lowerCaseAlphabets = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var upperCaseAlphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-  var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const lowerCaseAlphabets = "abcdefghijklmnopqrstuvwxyz";
+  const upperCaseAlphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numerals = "0123456789"
+  const specialChar = "/^[!@#$%^&*()_+\-=\[\]{};:\\|,.<>\/?]*$/";
 
-  var specialChar = ["/", "[", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "[", "]", "{", "}", ";", ":", "\\", "|", ",", ".", "<", ">", "/", "?", "]", "+", "/"]
+  // Generate password  based on User Input
 
- // Generate password  based on User Input
+  var listOfChar = lowerCaseAlphabets;
+  if (includeUpperCase) {
+    listOfChar =  listOfChar.concat(upperCaseAlphabets);
+  };
 
- var listOfChar = lowerCaseAlphabets;
- if (includeUpperCase) {
-   listOfChar =  listOfChar.concat(upperCaseAlphabets);
- };
+  if(includeNumbers) {
+    listOfChar = listOfChar.concat(numerals);
+  }
 
- if(includeNumbers) {
-   listOfChar = listOfChar.concat(numericArray);
- }
+  if(includeSpecicalCharacters){
+    listOfChar = listOfChar.concat(specialChar); 
+  }
 
- if(includeSpecicalCharacters){
-   listOfChar = listOfChar.concat(specialChar); 
- }
+  var maxNumber = listOfChar.length - 1;
+  console.log(maxNumber);
+  var minNumber = 0
+  var passwordChar = "";
 
- var maxNumber = listOfChar.length - 1;
- console.log(maxNumber);
- var minNumber = 0
- var passwordChar = "";
-
- for (var i=0; i<passwordLength; i++) {
-   charPosition = Math.floor(Math.random()*(maxNumber-minNumber) + minNumber);
-   
-   passwordChar =  passwordChar + listOfChar[charPosition];
- };
- console.log("the password is = ", passwordChar);
- return passwordChar;
-} 
+  for (var i=0; i<passwordLength; i++) {
+    charPosition = Math.floor(Math.random()*(maxNumber-minNumber) + minNumber);
+    
+    passwordChar =  passwordChar + listOfChar[charPosition];
+  };
+  console.log("the password is = ", passwordChar);
+  return passwordChar;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
