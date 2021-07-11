36c9bd72f5c8e1370b87059a237196cb1e94ff6c
Harry Shah <harvinder.shah@gmail.com>
2021-07-11 13:18:39 -0400

    screenshots added to README.md

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # password-generator
+
+## [Password Generator Url](https://harry-100.github.io/password-generator/)
+
+## [Project Url](https://github.com/harry-100/password-generator)
+
+## Brief Overview
+
+This application generates a unique customized password based on user preferences.
+Following are the key considerations:
+* the length of the password is between 8 and 128 characters.
+
+* the user is asked to choose if they want to include:
+    * lowercase alphabets
+    * uppercase alphabets
+    * numerals
+    * special characters
+
+* The program checks for the length of the password and that at least one option is selected for the character types to be included.
+
+* Based on the user inputs, a  customized password is generated.
+
+## Screenshots of the application
+
+![Prompt for asking questions](./assets/screenshots/screenshot-1.png)
+
+![Checking for password length](./assets/screenshots/screenshot-2.png)
+
+![Checking for options](./assets/screenshots/screenshot-3.png)
+
+![password generated](./assets/screenshots/screenshot-4.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,31 @@
 // Alerts and Prompts
 
 alert("Welcome to the World's most secure Password generator. Please click 'Generate Password' button");
+
+
 var generatePassword = function() {
   
   alert("You will be asked a series of questions to customize the password for you.");
-  var passwordLength = prompt("Please specify length of the password between 8 and 128 characters.");
+  passwordLength = prompt("Please enter a number between 8 and 128");
+  
+  // check if the input for password length is valid
 
-  if (passwordLength<8 || passwordLength>128) {
-    prompt("This is invalid number. Please enter a number between 8 and 128");
-  };
-  console.log(passwordLength);
+  for (var i = 0; i < 2; i++){
+    
+    if (passwordLength<8 || passwordLength>128) {
+      passwordLength = prompt("This is invalid number. Please enter a number between 8 and 128");
+      continue;
+      
+    }
+  }  
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Maximum Attempts exceeded. Please restart by clicking 'Generate Password' button");
+    return "";
+  }
+ 
+  // ask for various types of characters to be included in the password
 
+  var includeLowerCase = confirm("Do you want to include lower case?");
   var includeUpperCase = confirm("Do you want to include uppercase?");
   var includeNumbers = confirm("Do you want to include numbers")
   var includeSpecicalCharacters = confirm("Do you want to include special characters like ! # @ $ % ?");
@@ -25,22 +40,33 @@ var generatePassword = function() {
   const specialChar = "/^[!@#$%^&*()_+\-=\[\]{};:\\|,.<>\/?]*$/";
 
   // Generate password  based on User Input
-
-  var listOfChar = lowerCaseAlphabets;
+  var optionsCount = 0;
+  var listOfChar = "";
+  if(includeLowerCase) {
+    listOfChar = listOfChar.concat(lowerCaseAlphabets);
+    optionsCount = optionsCount + 1;
+  }
   if (includeUpperCase) {
     listOfChar =  listOfChar.concat(upperCaseAlphabets);
+    optionsCount = optionsCount + 1;
   };
 
   if(includeNumbers) {
     listOfChar = listOfChar.concat(numerals);
+    optionsCount = optionsCount + 1;
   }
 
   if(includeSpecicalCharacters){
-    listOfChar = listOfChar.concat(specialChar); 
+    listOfChar = listOfChar.concat(specialChar);
+    optionsCount = optionsCount + 1; 
+  }
+  // check if at least one option is selected
+  
+  if(optionsCount < 1) {
+    alert("You need to select at least one option to generate password. Please start over by clicking 'GENERATE PASSWORD' button.")
   }
 
   var maxNumber = listOfChar.length - 1;
-  console.log(maxNumber);
   var minNumber = 0
   var passwordChar = "";
 
@@ -49,7 +75,6 @@ var generatePassword = function() {
     
     passwordChar =  passwordChar + listOfChar[charPosition];
   };
-  console.log("the password is = ", passwordChar);
   return passwordChar;
 }
 
@@ -57,13 +82,14 @@ var generatePassword = function() {
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
 function writePassword() {
+  var passwordText = document.querySelector("#password");
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
-}
-
+ }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
